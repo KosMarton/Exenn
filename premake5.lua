1a28cd8bcd8c395119c9cb744eaeda8bfbe21efd
Márton Kós <kosmarci73@gmail.com>
2025-12-25 00:01:15 +0100

    Added Premake5 for project generation

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Exenn"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Exenn"
+	location "Exenn"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		buildoptions "/utf-8"
+
+		defines
+		{
+			"XN_PLATFORM_WINDOWS",
+			"XN_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "XN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "XN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "XN_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Exenn/vendor/spdlog/include",
+		"Exenn/src"
+	}
+
+	links
+	{
+		"Exenn"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		buildoptions "/utf-8"
+
+		defines
+		{
+			"XN_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "XN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "XN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "XN_DIST"
+		optimize "On"
